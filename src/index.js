f8a8e940ecd31d1f68c2aa72760a51824d66d387
Marcos Aurélio <marcosaureliolelo@outlook.com>
2020-05-12 17:27:42 -0300

    --Numbers

--- src/index.js
+++ src/index.js
@@ -12,6 +12,28 @@ var numberCode = 8987977;
 var a = 10, b = 9, c = 8;
 var language;
 
+// Aula 02 - Numbers
+
+var nota = 10;
+
+nota.toExponential(2); // 100
+nota.toFixed(2); // 10.00
+nota.toPrecision(1); // 1e+1
+nota.toString() // "10"
+nota.valueOf(); //10
+
+Math.abs(); // Valor absolute
+Math.floor(); // Valor inteiro
+Math.log(); // Logarítmo
+Math.min(); // Menor número
+Math.max(); // Maior número
+Math.pow(); // Potencia de número
+Math.random(); // Gera um número aleatório
+Math.round(); // Arredonda o número para o inteiro mais próximo
+Math.sin(); // Seno do número
+Math.sqrt(); // Raíz quadrada
+Math.tan(); // Tangente do número
+
 app.listen(300, () => {
     console.log(';) Server Online!')
 });
