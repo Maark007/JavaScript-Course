41267d4553b4bade5d9ae93720d78a0f0db89015
Marcos Aurélio <marcosaureliolelo@outlook.com>
2020-05-12 19:11:57 -0300

    --Strings

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
@@ -6,10 +6,12 @@ const app = express();
 
 var name = "AgileCode";
 var top10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var $scope = {title: "JavaScript", version: "6"};
+var $scope = { title: "JavaScript", version: "6" };
 var _person = "John";
 var numberCode = 8987977;
-var a = 10, b = 9, c = 8;
+var a = 10,
+  b = 9,
+  c = 8;
 var language;
 
 // Aula 02 - Numbers
@@ -19,7 +21,7 @@ var nota = 10;
 nota.toExponential(2); // 100
 nota.toFixed(2); // 10.00
 nota.toPrecision(1); // 1e+1
-nota.toString() // "10"
+nota.toString(); // "10"
 nota.valueOf(); //10
 
 Math.abs(); // Valor absolute
@@ -34,6 +36,24 @@ Math.sin(); // Seno do número
 Math.sqrt(); // Raíz quadrada
 Math.tan(); // Tangente do número
 
-app.listen(300, () => {
-    console.log(';) Server Online!')
+// Aula 03 - Strings
+
+var nome = "AgileCode";
+
+nome.charAt(2); // = 'i' => Retorna o char da posição
+nome.charCodeAt(0); // = 65 => Retorna o codigo do char
+nome.concat("!"); // = 'AgileCode!' => Concatena as strings
+nome.indexOf("e"); // = 4 => retorna o indice da ocorrencia do char
+nome.replace("Code", "!"); // = "Agile!" =>  Substitui parte da String por outra
+nome.split("e"); // = ['Agil', 'Cod'] => Divide a string
+
+nome.length; // = 9 => Tamanho da String
+nome.toUpperCase; // = AGILECODE => Gera uma nova string em letra maiúscula
+nome.toLowerCase; // = agilecode => Gera uma nova string em letra minúscula
+nome.trim; // Remove os espaços em branco
+nome.substring(0, 5); // = Agile => Extrai a parte da string desejada
+nome.search("g"); // = 1 =Retorna a posição da string
+
+app.listen(3000, () => {
+  console.log();
 });
